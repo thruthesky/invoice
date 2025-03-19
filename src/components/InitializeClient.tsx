c11755b1358dee19d189f4e108abeb1b2b0d1443
Charles <withcenter.dev3@gmail.com>
2025-03-19 09:12:38 +0800

    update on k

--- src/components/InitializeClient.tsx
+++ src/components/InitializeClient.tsx
@@ -14,13 +14,13 @@ export default function InitializeClient({
 
   // Your web app's Firebase configuration
   const firebaseConfig = {
-    apiKey: "AIzaSyBVp4ERqSY3dfQctY700eBdcbXmSZRPJEU",
-    authDomain: "withcenter-test-4.firebaseapp.com",
-    databaseURL: "https://withcenter-test-4-default-rtdb.firebaseio.com",
-    projectId: "withcenter-test-4",
-    storageBucket: "withcenter-test-4.appspot.com",
-    messagingSenderId: "109766947030",
-    appId: "1:109766947030:web:adea06b4bee7a666239977",
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
   };
 
   const [app, setApp] = useState<FirebaseApp | null>(null);
