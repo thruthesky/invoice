3dd07313da60ed39d47b4840ffdae0b4b23c3dd8
JaeHo Song <thruthesky@gmail.com>
2025-04-09 14:02:28 +0800

    remove the react component

--- src/withcenter-react-components/Description.tsx
+++
@@ -1,9 +0,0 @@
-export default function Description({
-  children,
-  className = "",
-}: {
-  children: React.ReactNode;
-  className?: string;
-}) {
-  return <div className={`mt-3 text-sm text-gray-600 ${className}`}>{children}</div>;
-}
--- src/withcenter-react-components/Title.tsx
+++
@@ -1,9 +0,0 @@
-export default function Title({
-  children,
-  className = "",
-}: {
-  children: React.ReactNode;
-  className?: string;
-}) {
-  return <h1 className={`mt-3 text-3xl font-semibold ${className}`}>{children}</h1>;
-}
