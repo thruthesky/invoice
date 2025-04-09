6d1ab298c30ce3674f51672a260b2971e379127d
JaeHo Song <thruthesky@gmail.com>
2025-04-09 11:10:34 +0800

    update page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,10 +15,10 @@ export default function Home() {
     router.push(`/chat?ask=${prompt}`);
   }
   return (
-    <section >
-      <header className="flex justify-start gap-5 items-center p-4 bg-gray-800 text-white" >
+    <section>
+      <header className="flex justify-start gap-5 items-center p-4 bg-gray-800 text-white">
         <h1>InvoiceGen</h1>
-      </header >
+      </header>
       <section className="flex flex-col gap-5 p-5">
         <h2>Welcome to InvoiceGen</h2>
 
@@ -27,21 +27,35 @@ export default function Home() {
         <h2>Choose what you want to build:</h2>
 
         <nav className="flex gap-3">
-          <button onClick={() => openChat({ type: "forum-app" })}>Forum app</button>
-          <button onClick={() => openChat({ type: "chat-app" })}>Chat app</button>
-          <button onClick={() => openChat({ type: "shopping-mall-app" })}>Shopping mall</button>
-          <button onClick={() => openChat({ type: "game-app" })}>Game app</button>
-          <button onClick={() => openChat({ type: "fitness-app" })}>Fitness app</button>
+          <button onClick={() => openChat({ type: "forum-app" })}>
+            Forum app
+          </button>
+          <button onClick={() => openChat({ type: "chat-app" })}>
+            Chat app
+          </button>
+          <button onClick={() => openChat({ type: "shopping-mall-app" })}>
+            Shopping mall
+          </button>
+          <button onClick={() => openChat({ type: "game-app" })}>
+            Game app
+          </button>
+          <button onClick={() => openChat({ type: "fitness-app" })}>
+            Fitness app
+          </button>
         </nav>
 
         <section className="mt-12">
           <h3 className="h3">Or write down your needs.</h3>
           <form onSubmit={onSubmit} className="flex flex-col gap-4">
-            <input name="prompt" type="text" placeholder="Write your needs here..." />
+            <input
+              name="prompt"
+              type="text"
+              placeholder="Write your needs here..."
+            />
             <button>Send</button>
           </form>
         </section>
       </section>
-    </section >
+    </section>
   );
 }
